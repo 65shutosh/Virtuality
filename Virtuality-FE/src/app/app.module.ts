cb952ff6220e2da7f5920094d999e9aa29ce7657
65shutosh <42913557+65shutosh@users.noreply.github.com>
2020-05-18 18:37:50 +0530

    commit_problem

--- Virtuality-FE/src/app/app.module.ts
+++ Virtuality-FE/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 
@@ -21,18 +21,6 @@ import { LoginComponent } from './login/login.component';
    bootstrap: [
       AppComponent
    ]
-=======
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
->>>>>>> ed2a6ca3076c19042e040bce5a21a9bbdb4f10ef
 })
 export class AppModule { }
