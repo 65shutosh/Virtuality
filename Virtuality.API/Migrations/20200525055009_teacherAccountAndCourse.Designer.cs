48c3bbb150338921d897f135325d2e0b98e25d8e
65shutosh <42913557+65shutosh@users.noreply.github.com>
2020-05-25 11:27:20 +0530

    ef migration

--- Virtuality.API/Migrations/20200525055009_teacherAccountAndCourse.Designer.cs
+++ Virtuality.API/Migrations/20200525055009_teacherAccountAndCourse.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Virtuality.API.Data;
 
 namespace Virtuality.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200525055009_teacherAccountAndCourse")]
+    partial class teacherAccountAndCourse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
